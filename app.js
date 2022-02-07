d329ceeead43f27f4fc936f93129494c57574cce
immemx <eayresm4@gmail.com>
2022-02-06 23:14:44 -0500

    question validation

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub Username');
+                return false;
+            }
+        }
       },
       {
         type: 'input',
@@ -47,12 +63,28 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        validate: projectNameInput => {
+            if (projectNameInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of your project!');
+                return false;
+            }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: projectDescription => {
+            if (projectDescription) {
+                return true;
+            } else {
+                console.log('Please enter a description of the project!');
+                return false;
+            }
+        }
       },
       {
         type: 'checkbox',
@@ -63,7 +95,15 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: link => {
+            if (link) {
+                return true;
+            } else {
+                console.log('Please enter the link to your GitHub project!');
+                return false;
+            }
+        }
       },
       {
         type: 'confirm',
